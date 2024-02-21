cc79046a535b5c71abdab51ce9e3739b6f7a0bb0
equaer <vriyazzz1001@gmail.com>
2024-02-21 11:30:47 +0300

    добавление view для дешифровки

---
+++ ViewModels/Decoder.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Caesar_decoder_encoder.ViewModels
+{
+    /// <summary>
+    /// Логика взаимодействия для Decoder.xaml
+    /// </summary>
+    public partial class Decoder : Window
+    {
+        public Decoder()
+        {
+            InitializeComponent();
+        }
+    }
+}
